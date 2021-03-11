7b409e07f51c70fa14cd4076256ae99bd97cb917
Manicardi <gabrieltrovatto@hotmail.com>
2021-03-11 19:20:36 -0300

    Category

---
+++ src/main/java/com/gabrielmanicardi/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielmanicardi.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielmanicardi.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
